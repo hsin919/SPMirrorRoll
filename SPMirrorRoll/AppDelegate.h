6fce1d22e618d562cbac383c6073b1ef62cb3fc6
hsin <hsin919@gmail.com>
2015-02-10 09:38:46 +0800

    Initial Commit

---
+++ SPMirrorRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPMirrorRoll
+//
+//  Created by Nathan on 2015/2/10.
+//  Copyright (c) 2015年 Stupid Pencil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
